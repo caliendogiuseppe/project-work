a552588e300f1caf11f5611e794bd1c7c39972dd
caliendogiuseppe <caliendogiuseppe3@gmail.com>
2025-08-08 22:02:18 +0200

    changed host in frontend

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    HOST: "http://localhost:3000", //https://project-work-32j4.onrender.com http://localhost:3000
+    HOST: "https://project-work-32j4.onrender.com", //https://project-work-32j4.onrender.com http://localhost:3000
     API_REPORTS_URL: "/api/reports/",
     STATIC_FOLDER_URL: "/public/",
     DIRECT_DONWLOAD_URL: "/download/",
